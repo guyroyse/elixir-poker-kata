8d41a87c31454f21fa342549e0bb706353ff23f3
Guy Royse <guy@guyroyse.com>
2019-09-21 18:30:58 -0400

    recognizes matched ranks of all flavors

--- lib/poker/hand.ex
+++ lib/poker/hand.ex
@@ -8,17 +8,38 @@ defmodule Hand do
 
   def rank(%Hand{} = hand) do
     cond do
+      is_five_of_a_kind?(hand.cards) -> HandRank.five_of_a_kind
+      is_four_of_a_kind?(hand.cards) -> HandRank.four_of_a_kind
+      is_full_house?(hand.cards) -> HandRank.full_house
+      is_three_of_a_kind?(hand.cards) -> HandRank.three_of_a_kind
+      is_two_pair?(hand.cards) -> HandRank.two_pair
       is_pair?(hand.cards) -> HandRank.pair
       is_high_card?(hand.cards) -> HandRank.high_card
     end
   end
 
+  defp is_five_of_a_kind?(cards) do
+    RankCounter.count_quints(cards) == 1
+  end
+
+  defp is_four_of_a_kind?(cards) do
+    RankCounter.count_quads(cards) == 1
+  end
+
+  defp is_full_house?(cards) do
+    RankCounter.count_triples(cards) == 1 && RankCounter.count_pairs(cards) == 1
+  end
+
+  defp is_three_of_a_kind?(cards) do
+    RankCounter.count_triples(cards) == 1
+  end
+
+  defp is_two_pair?(cards) do
+    RankCounter.count_pairs(cards) == 2
+  end
+
   defp is_pair?(cards) do
-    cards
-    |> Enum.map(&(&1.rank))
-    |> Enum.group_by(&(&1))
-    |> Enum.map(fn { _, cards } -> Enum.count(cards) end)
-    |> Enum.member?(2)
+    RankCounter.count_pairs(cards) == 1
   end
 
   defp is_high_card?(_) do
@@ -30,6 +51,40 @@ defmodule Hand do
     |> Enum.sort(&(Card.compare(&1, &2) == :gt))
     |> Enum.map(&(Card.to_string(&1)))
     |> Enum.join(" ")
+  end
+
+end
+
+defmodule RankCounter do
+
+  def count_pairs(cards), do: count_matches(cards, 2)
+  def count_triples(cards), do: count_matches(cards, 3)
+  def count_quads(cards), do: count_matches(cards, 4)
+  def count_quints(cards), do: count_matches(cards, 5)
+
+  def count_matches(cards, size) do
+    cards
+    |> just_the_ranks()
+    |> count_the_ranks()
+    |> just_the_matches_by_size(size)
+    |> Enum.count()
+  end
+
+  def just_the_ranks(cards) do
+    cards
+    |> Enum.map(fn card -> card.rank.rank end)
+  end
+
+  def count_the_ranks(cards) do
+    cards
+    |> Enum.group_by(&(&1))
+    |> Enum.map(fn { rank, cards } -> { rank, Enum.count(cards) } end)
+    |> Enum.into(%{})
+  end
+
+  def just_the_matches_by_size(histogram, size) do
+    histogram
+    |> Enum.filter(fn { _rank, count } -> count == size end)
   end
 
 end
--- test/hand_test.exs
+++ test/hand_test.exs
@@ -19,6 +19,46 @@ defmodule HandTest do
         Card.new(Rank.two, Suit.clubs)
       ]),
 
+      two_pair_aces_and_eights: Hand.new([
+        Card.new(Rank.ace, Suit.spades),
+        Card.new(Rank.ace, Suit.diamonds),
+        Card.new(Rank.eight, Suit.clubs),
+        Card.new(Rank.eight, Suit.hearts),
+        Card.new(Rank.two, Suit.clubs)
+      ]),
+
+      three_kings: Hand.new([
+        Card.new(Rank.king, Suit.spades),
+        Card.new(Rank.king, Suit.diamonds),
+        Card.new(Rank.king, Suit.clubs),
+        Card.new(Rank.seven, Suit.clubs),
+        Card.new(Rank.two, Suit.clubs)
+      ]),
+
+      full_house: Hand.new([
+        Card.new(Rank.king, Suit.spades),
+        Card.new(Rank.king, Suit.diamonds),
+        Card.new(Rank.king, Suit.clubs),
+        Card.new(Rank.two, Suit.hearts),
+        Card.new(Rank.two, Suit.clubs)
+      ]),
+
+      four_ladys: Hand.new([
+        Card.new(Rank.queen, Suit.spades),
+        Card.new(Rank.queen, Suit.diamonds),
+        Card.new(Rank.queen, Suit.clubs),
+        Card.new(Rank.queen, Suit.hearts),
+        Card.new(Rank.two, Suit.clubs)
+      ]),
+
+      five_aces: Hand.new([
+        Card.new(Rank.ace, Suit.spades),
+        Card.new(Rank.ace, Suit.diamonds),
+        Card.new(Rank.ace, Suit.clubs),
+        Card.new(Rank.ace, Suit.hearts),
+        Card.new(Rank.ace, Suit.clubs)
+      ]),
+
       unranked: Hand.new([
         Card.new(Rank.seven, Suit.clubs),
         Card.new(Rank.eight, Suit.clubs),
@@ -35,6 +75,26 @@ defmodule HandTest do
 
   test "it ranks a pair", context do
     assert Hand.rank(context.pair_of_aces) == HandRank.pair
+  end
+
+  test "it ranks two pair", context do
+    assert Hand.rank(context.two_pair_aces_and_eights) == HandRank.two_pair
+  end
+
+  test "it ranks three of a kinda", context do
+    assert Hand.rank(context.three_kings) == HandRank.three_of_a_kind
+  end
+
+  test "it ranks a full house", context do
+    assert Hand.rank(context.full_house) == HandRank.full_house
+  end
+
+  test "it ranks four of a kinda", context do
+    assert Hand.rank(context.four_ladys) == HandRank.four_of_a_kind
+  end
+
+  test "it ranks five of a kinda", context do
+    assert Hand.rank(context.five_aces) == HandRank.five_of_a_kind
   end
 
   test "it turns an unranked hand into a string", context do
